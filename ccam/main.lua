7589c6a0e9ba7b2000733a9324511abdfa4eb87b
ACharLuk <acharluk@gmail.com>
2016-08-03 01:26:57 +0200

    Refractoring

--- ccam/main.lua
+++ ccam/main.lua
@@ -3,45 +3,42 @@ local args = { ... }
 
 local usage = "Usage: ccam <option> [application]"
         .. "\nAvaliable options:"
-        .. "\n install to install an app"
-        .. "\n update to update an app"
+        .. "\n install <name> to install an app"
+        .. "\n update <name> to update an app"
         .. "\n updateall to update all apps and libraries"
-        .. "\n remove to remove an app"
+        .. "\n remove <name> to remove an app"
 
-if #args < 2 and args[1] ~= "updateall" then
-	print(usage)
-elseif #args == 2 then
+if #args == 2 then
 	local option = args[1]
     local app_name = args[2]
     
 
 	if option == "install" then
-		ccam.downloadApp(app_name)
+		ccam.download(app_name)
         
 	elseif option == "update" then
-		ccam.updateApp(app_name)
+		ccam.update(app_name)
         
 	elseif option == "remove" then
-		ccam.deleteApp(app_name)
+		ccam.delete(app_name)
         
 	else
 		print(usage)
 	end
 
-elseif #args < 2 and args[1] == "updateall" then
-	local apps = fs.list(CCAM_CONF.APP_DIR)
-	local libs = fs.list(CCAM_CONF.LIB_DIR)
-
+elseif #args == 1 and args[1] == "updateall" then
 	-- Update apps
-	for _, v in pairs(apps) do
+	for _, v in pairs(fs.list(CCAM_CONF.APP_DIR)) do
 		print("Updating app: " .. v)
-		ccam.updateApp(v, false)
+		ccam.update(v, false)
 	end
 
 	-- Update libs
-	for _, v in pairs(libs) do
+	for _, v in pairs(fs.list(CCAM_CONF.LIB_DIR)) do
 		print("Updating lib: " .. v)
-		ccam.updateApp(v, true)
+		ccam.update(v, true)
 	end
 
+else
+	print(usage)
 end
