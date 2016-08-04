2eb1b3b2d032f57d9e8dcdc01eea3e9fc9a87447
ACharLuk <acharluk@gmail.com>
2016-08-04 07:33:29 +0200

    Refractoring

--- ccam/main.lua
+++ ccam/main.lua
@@ -1,26 +1,26 @@
 --[[ CCAM App ]]--
 local args = { ... }
 
-local usage = "Usage: ccam <option> [application]"
+local usage = "Usage: ccam <option> [resource]"
         .. "\nAvaliable options:"
-        .. "\n install <name> to install an app"
-        .. "\n update <name> to update an app"
+        .. "\n install <name>"
+        .. "\n remove <name>"
+        .. "\n update <name>"
         .. "\n updateall to update all apps and libraries"
-        .. "\n remove <name> to remove an app"
 
 if #args == 2 then
 	local option = args[1]
-    local app_name = args[2]
+    local resource = args[2]
     
 
 	if option == "install" then
-		ccam.download(app_name)
+		ccam.download(resource)
         
 	elseif option == "update" then
-		ccam.update(app_name)
+		ccam.update(resource)
         
 	elseif option == "remove" then
-		ccam.delete(app_name)
+		ccam.delete(resource)
         
 	else
 		print(usage)
@@ -29,13 +29,13 @@ if #args == 2 then
 elseif #args == 1 and args[1] == "updateall" then
 	-- Update apps
 	for _, v in pairs(fs.list(CCAM_CONF.APP_DIR)) do
-		print("Updating app: " .. v)
+		print("\nUpdating app: " .. v)
 		ccam.update(v, false)
 	end
 
 	-- Update libs
 	for _, v in pairs(fs.list(CCAM_CONF.LIB_DIR)) do
-		print("Updating lib: " .. v)
+		print("\nUpdating lib: " .. v)
 		ccam.update(v, true)
 	end
 
